8860a018e45411716e023b68cb19fc550f1835da
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2020-12-11 15:43:38 +0900

    feat : implement userList MVC with model (#1)

--- src/main/java/com/sexyguys/suhang/domain/User.java
+++ src/main/java/com/sexyguys/suhang/domain/User.java
@@ -1,46 +1,56 @@
 package com.sexyguys.suhang.domain;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "num")
     int id;
     String email;
     String password;
     String school;
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setSchool(String school) {
-        this.school = school;
-    }
+    String salt;
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getSchool() {
         return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
     }
 }
