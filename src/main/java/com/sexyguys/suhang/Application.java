0b4eac561310260a0371ed9138079a7acb28abcb
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2020-12-11 11:56:32 +0900

    feat : add database.properties file to project

--- src/main/java/com/sexyguys/suhang/Application.java
+++ src/main/java/com/sexyguys/suhang/Application.java
@@ -2,8 +2,10 @@ package com.sexyguys.suhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:config/database.properties"})
 public class Application {
 
     public static void main(String[] args) {
