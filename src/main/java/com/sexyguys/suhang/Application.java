739872a7750f48137ecd41ca81e9c35449f6d844
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2020-12-11 11:23:58 +0900

    chore : change main Application file name

--- src/main/java/com/sexyguys/suhang/Application.java
+++ src/main/java/com/sexyguys/suhang/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationProgrammingApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationProgrammingApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
