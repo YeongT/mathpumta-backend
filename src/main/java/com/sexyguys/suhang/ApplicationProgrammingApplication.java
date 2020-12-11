fbcc653a8f37d1d9bb5a84a0b2c096783a6e6b23
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2020-12-11 11:15:00 +0900

    init : init project with springInitializer

---
+++ src/main/java/com/sexyguys/suhang/ApplicationProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.sexyguys.suhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sexyguys/suhang/ApplicationProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sexyguys.suhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
